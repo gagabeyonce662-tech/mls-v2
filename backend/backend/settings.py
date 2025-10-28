e8410144c7b6bcf7bc4f32c97854f85c7bb284a8
AbhishekSrivastav <abhisheksrivastav596@gmail.com>
2025-10-28 20:34:48 +0530

    cors headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     "ckeditor_uploader",
     "vlog",  
     "mls",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -149,3 +151,10 @@ CKEDITOR_CONFIGS = {
 
 CLIENT_ID = 'uCyucNP7QrV4fLA8YtWngTUg'
 CLIENT_SECRET = 'OFocUcAnQShbZRPUKdaafgEi'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
