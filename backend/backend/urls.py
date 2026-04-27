ead3a7e5ab50b1f290b630a7e29a61d967e633c3
Abhijeet-ravan <abhijeet@ravan.ai>
2026-04-27 21:31:48 +0530

    backend defaults to admin route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')), 
     path('api/vlog/',include('vlog.urls')),
